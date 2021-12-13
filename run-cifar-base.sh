5a5657118d417945b0a1e2f6403504e9a9adf57f
Lingren Kong <47297555+LingrenKong@users.noreply.github.com>
2021-12-13 16:01:11 +0800

    running snips
    running snips

---
+++ run-cifar-base.sh
@@ -0,0 +1,2 @@
+python train_models.py data_option=cifar10 model_option=VGG13 t1=10 R=1
+python minority_collapse.py data_option=cifar10 model_option=VGG13 t1=10 R=1
---
+++ run-mnist-base.sh
@@ -0,0 +1,2 @@
+python train_models.py data_option=fashion_mnist model_option=VGG13 t1=10 R=1
+python minority_collapse.py data_option=fashion_mnist model_option=VGG13 t1=10 R=1
